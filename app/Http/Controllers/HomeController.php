3493d66381bee987da1737afbe5e52220a15e5c1
ohyeontaeg <duduio2050@gmail.com>
2020-05-20 17:42:15 +0900

    주문모델 이름 변경, 주문 시 데이터 베이스에 저장, 나의 주문내역 기록페이지 back-end, user table에 전화번호,주소 추가

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\receive;
 use Illuminate\Http\Request;
+
 
 class HomeController extends Controller
 {
@@ -19,7 +21,18 @@ class HomeController extends Controller
             'term' => 'required',
             'mobile' => 'required'
         ]);
+        $newReceive = new receive();
+        $newReceive->name = $request->name;
+        $newReceive->currentaddress = $request->currentaddress;
+        $newReceive->term = $request->term;
+        $newReceive->mobile = $request->mobile;
+        $newReceive->save();
+        return redirect()->route('towel');
+    }
 
-
+    public function usage()
+    {
+        $usage = receive::paginate();
+        return view('towel', compact('usage'));
     }
 }
